0eadb1c8021112fe854da13f3e59d8e76d9c9447
jorge-j1m <jorge.jim@icloud.com>
2025-10-20 21:37:37 -0400

    feat: enable employee auth and update TypeScript module settings

--- apps/admin-panel/src/app/layout.tsx
+++ apps/admin-panel/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-// import { client } from "@repo/employee-auth";
+import { client } from "@repo/employee-auth";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -26,8 +26,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        // className={`${geistSans.variable} ${geistMono.variable} antialiased`} id={client}
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`} id={client}
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
